7f738fc268f9ef3586ebbcf56d5ce7cffbbf488f
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2021-05-23 21:40:02 +0200

    Adding new logic of program
    After experiments and collecting new data we have decided to abort previous code and made some fixes. Now code will work differently

--- dataset_class.py
+++ dataset_class.py
@@ -1,40 +1,14 @@
-class Small_Dataset:
-    def __init__(self, data, data_path, parent_name):
-        self.hamming_window = None
-        self.data_chunks = None
-        self.data = data
-        self.parent_name = parent_name
+import numpy as np
 
-        # Checking if frequency is predefined
-        if "kHz" in data_path:
-            if "100" in data_path:
-                self.frequency = 100
-                #print(f'SET FREQ 100 in {self.parent_name} (this is not a default value)')
-            elif "200" in data_path:
-                self.frequency = 200
-                #print(f'SET FREQ 200 in {self.parent_name} (this is not a default value)')
-            elif "400" in data_path:
-                self.frequency = 400
-                #print(f'SET FREQ 400 in {self.parent_name} (this is not a default value)')
-        # Setting frequency 400 by default
-        else:
-            self.frequency = 400
-        self.len = len(data)
+class Dataset:
+    def __init__(self, data):
+        self.data_from_csv = data
+        self.hamming_window_list = []
 
-    def get_chunks(self):
-        return self.data_chunks
-
-    def get_len(self):
-        return self.len
-
-    def get_divider_for_chunks(self):
-        return self.frequency*2
-
-    def get_data(self):
-        return self.data
-
-    def set_chunks(self, chunk):
-        self.data_chunks = chunk
-
-    def set_hamming_window(self, data):
-        self.hamming_window = data
+    def computer_hamming_window(self):
+        for item in self.data_from_csv:
+            temporary_hamming_window = []
+            current_array = np.array(item)
+            hamming = np.hamming(len(item))
+            temporary_hamming_window.append(current_array * hamming)
+            self.hamming_window_list.append(temporary_hamming_window)
--- main.py
+++ main.py
@@ -1,50 +1,13 @@
 import functions
+import dataset_class
 
+# Loading files from csv.
+# Function functions.get_data_from_csv() requires type of data set [healthy or faulty] and return list of pandas DataFrame objects
+# Returned data from function above is now assigned as data_from_csf parameter in dataset_class.Dataset()
+healthy = dataset_class.Dataset(functions.get_data_from_csv(type="healthy"))
+faulty = dataset_class.Dataset(functions.get_data_from_csv(type="faulty"))
 
-# functions.data_names_sorting() ~ HOW IT WORKS
-# Returning dictionary of sorted fields names
-files_names = functions.data_names_sorting()
-
-# functions.fulfill_main_data_set_container() ~ HOW IT WORKS
-# Putting data into specific container and all that containers into one main container
-# For example main_dataset_container[] -> healthy 5A[] -> data_from_csv[]
-main_dataset_container = functions.fulfill_main_data_set_container(files_names=files_names)
-
-# functions.divide_into_chunks() ~ HOW IT WORKS
-# Dividing all data from each data set into smaller list (chunks)
-# First of all argument for divide_into_chunks is list inside main_dataset_container ex. main_dataset_container[] -> healthy 5A[]
-# Then for each list inside (ex. healthy 5A[]) is created chunk of data (specific number of data in list), this is dependent of sampling frequency (default is 400)
-# Then for each item (Small_Dataset ex. main_dataset_container[] -> healthy 5A[] -> data_from_csv[sets of data HERE]), parameter chunk is set. Basically from 1 long list (ex 2000 records) there are created 3 smaller lists.
-# At the end we have ex. main_dataset_container[] -> healthy 5A[] -> 5A Healthy4.csv_data[] -> chunks[] if there was 2000 records inside a 5A Healthy4.csv_data[] now ifs a list containing for example 3 or 4 list that together contain 2000 records
-
-# VARIABLE DESCRIPTION
-# container is list of specific type csv files ex. healthy 5A: ['5A Healthy 4.csv', '5A Healthy 5.csv', '5A Healthy 1.csv', '5A Healthy 2.csv', '5A Healthy 3.csv']
-# Each file inside container is 1 single file with big number of records (aprox. 2k)
-# In this step we are converting this 1 big list into couple smaller lists (CHUNKS),
-for container in main_dataset_container:
-    functions.divide_into_chunks(container.get_data_from_csv())
-
-# set_merged_chunks() ~ HOW IT WORKS
-# Merging chunks into one list for each main dataset.
-# Now main_dataset_container have specific type containers that contain all merged chunks from each csv file
-# ex. main_dataset_container[] -> healthy 5A[] ->  \all merged chunks into one\
-for container in main_dataset_container:
-    container.set_merged_chunks()
-
-
-
-# Making hamming window from chunks that were previously created
-# healthy_data_5A = make_hamming_window(healthy_data_5A)
-# healthy_data_10A = make_hamming_window(healthy_data_10A)
-# faulty_data_5A = make_hamming_window(faulty_data_5A)
-# faulty_data_10A = make_hamming_window(faulty_data_10A)
-# distorted_data_5A = make_hamming_window(distorted_data_5A)
-# distorted_data_10A = make_hamming_window(distorted_data_10A)
-
-
-
-
-
-
-
-
+# Now hamming window function is being computed for each item in data_from_csv parameter od Dataset()
+# Elements are saved in new list (Dataset() parameter hamming_window_list )
+healthy.computer_hamming_window()
+faulty.computer_hamming_window()
